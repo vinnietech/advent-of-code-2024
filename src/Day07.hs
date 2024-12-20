20dd8d791c6b9bf8641912344f91ba72bcb87b5b
Vincent Verbrugh <vincent@verbrugh.nl>
2024-12-20 16:01:06 +0100

    Day 7: Bridge Repair

--- src/Day07.hs
+++ src/Day07.hs
@@ -1,10 +1,33 @@
 module Day07 where
 
+import Data.Maybe (catMaybes)
+
 import Paths_aoc2024 (getDataFileName)
+
+parseInput :: String -> ([(Int, [Int])])
+parseInput =
+  map (\line ->
+    let (k, v) = break (== ':') line
+        key = read k
+        values = map read (words (drop 1 v))
+    in (key, values))
+  . lines
+
+calculateAllValues :: [Int] -> [Int]
+calculateAllValues [] = []
+calculateAllValues [x] = [x]
+calculateAllValues (x:xs) =
+  foldl (\acc y -> [a + y | a <- acc] ++ [a * y | a <- acc] ++ [concatInts a y | a <- acc]) [x] xs
+
+concatInts :: Int -> Int -> Int
+concatInts x y = read (show x ++ show y)
 
 day07 :: IO ()
 day07 = do
   inputLines <- lines <$> (getDataFileName "day07-input.txt" >>= readFile)
-  putStrLn "This is what I read from input:"
-  putStrLn $ unlines inputLines
-  putStrLn "TODO: implement Day 07"
+
+  let equations = parseInput (unlines inputLines)
+  -- putStrLn $ "Equations: " ++ show equations
+
+  let part1 = sum $ catMaybes $ map (\(target, nums) -> if target `elem` calculateAllValues nums then Just target else Nothing) equations
+  putStrLn $ "Part 1: " ++ show part1
